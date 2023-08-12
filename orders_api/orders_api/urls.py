353761b7c924195125885c7b13a24632fee7a77c
Francis Anthony Luna <FrancisAnthony.Luna@dyson.com>
2023-08-13 01:38:49 +0800

    Update django views and url conf

--- orders_api/orders_api/urls.py
+++ orders_api/orders_api/urls.py
@@ -1,31 +1,38 @@
 """
-Url module
+Django URLs
 """
-from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
+from .views import (AvailableProducts, CreateOrder, DeductProductQuantity,
+                    ListProducts, ProductDetail, UnavailableProducts,
+                    UpdateProductStatus, api_root)
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "is_staff"]
+schema_url_patterns = [
+    path("products/", include("orders_api.urls")),
+    path("orders/", include("orders_api.urls")),
+]
 
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r"users", UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path(
+        "openapi",
+        get_schema_view(
+            title="Products API", description="API for managing orders", version="1.0.0", patterns=schema_url_patterns
+        ),
+        name="openapi-schema",
+    ),
+    path("", api_root),
+    path("products/", ListProducts.as_view(), name="products-list"),
+    path("products/available", AvailableProducts.as_view(), name="available-products-list"),
+    path("products/unavailable", UnavailableProducts.as_view(), name="unavailable-products-list"),
+    path("products/<int:pk>/", ProductDetail.as_view(), name="product-detail"),
+    path("products/<int:pk>/deductstock", DeductProductQuantity.as_view(), name="deduct-product-quantity"),
+    path("products/<int:pk>/status", UpdateProductStatus.as_view(), name="update-product-status"),
+    path("orders/", CreateOrder.as_view(), name="create-order"),
+    path(
+        "swagger-ui/",
+        TemplateView.as_view(template_name="swagger-ui.html", extra_context={"schema_url": "openapi-schema"}),
+        name="swagger-ui",
+    ),
 ]
